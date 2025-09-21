f866f97d863c3057aaf6d2f006e34edaf0d7235b
PJutch <103033969+PJutch@users.noreply.github.com>
2025-09-22 01:10:33 +0300

    Actually finish test scenario

--- library/main.go
+++ library/main.go
@@ -1,6 +1,11 @@
 package main
 
-// Scenario to test the library bcs idk how to do unit-tests here
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Предположим, что в этом задании надо сделать так
 func main() {
 	books := []Book{
 		{Title: "Война и Мир", Author: "Лев Николаевич Толстой", PublicationYear: 1865},
@@ -15,5 +20,37 @@ func main() {
 
 	for _, book := range books {
 		library.AddBook(book)
+	}
+
+	if book, ok := library.GetBook("Конец вечности"); ok {
+		fmt.Println(book)
+	} else {
+		panic("this book should be found")
+	}
+
+	library.SetIdGenerator(func(title Book, bookStorage BookStorage) BookId {
+		return BookId(rand.Int())
+	})
+
+	// но ведь смеа генератора id никак не влияет на это
+	if book, ok := library.GetBook("Сильмариллион"); ok {
+		fmt.Println(book)
+	} else {
+		panic("this book should be found")
+	}
+
+	library.SetStorage(MakeBookStorage())
+
+	library.AddBook(Book{Title: "Преступление и наказание", Author: "Фёдор Михайлович Достоевский", PublicationYear: 1866})
+	library.AddBook(Book{Title: "Лев, Колдунья и Платяной шкаф", Author: "Клайвом Стэйплзом Льюисом", PublicationYear: 1950})
+
+	if _, ok := library.GetBook("Сильмариллион"); ok {
+		panic("this book should not be found")
+	}
+
+	if book, ok := library.GetBook("Преступление и наказание"); ok {
+		fmt.Println(book)
+	} else {
+		panic("this book should be found")
 	}
 }
