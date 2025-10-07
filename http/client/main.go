f6ab4ed427b70c62987a05c46b210e87ef9ae3f4
PJutch <103033969+PJutch@users.noreply.github.com>
2025-10-08 01:12:11 +0300

    Add a decode request to a client

--- http/client/main.go
+++ http/client/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -23,10 +25,53 @@ func GetVersion(url string) (string, error) {
 	return string(body), nil
 }
 
+type jsonInput struct {
+	InputString string `json:"inputString"`
+}
+
+type jsonOutput struct {
+	OutputString string `json:"outputString"`
+}
+
+func SendDecodeRequest(url string, input string) (string, error) {
+	reqBody, err := json.Marshal(jsonInput{input})
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	var output jsonOutput
+	if err := json.Unmarshal(respBody, &output); err != nil {
+		fmt.Println(err)
+		return "", nil
+	}
+
+	return string(output.OutputString), nil
+}
+
 func main() {
 	version, err := GetVersion("http://:8080/version")
 	if err == nil {
 		fmt.Println(version)
+	} else {
+		fmt.Println(err)
+	}
+
+	decoded, err := SendDecodeRequest("http://:8080/decode", "dGVzdA==")
+	if err == nil {
+		fmt.Println(decoded)
 	} else {
 		fmt.Println(err)
 	}
